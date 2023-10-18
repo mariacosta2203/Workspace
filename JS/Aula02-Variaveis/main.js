f851c3ef8dc409a6c051818a8aec8ec3d84fd26c
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-10-17 21:51:18 -0300

    segunda aula de js

---
+++ JS/Aula02-Variaveis/main.js
@@ -0,0 +1,43 @@
+// var nomeDoPrimeiroAluno = "Eduardo"
+
+// console.log(nomeDoPrimeiroAluno)
+
+// let numeroDeAprovados = 5;
+
+// numeroDeAprovados = 7
+
+// console.log(numeroDeAprovados)
+
+// console.clear()
+
+// var nomeAluno = "Felipe"
+
+// nomeAluno = "Victor"
+
+// console.log(nomeAluno)
+
+// const nomeSiteWeb = "facebook"
+
+// nomeSiteWeb = "facebak"
+
+// var numero01 = 15;
+
+// var numero02 = 8;
+
+// var soma = numero01 + numero02;
+
+// console.log(soma)
+
+// var texto = "A proporção de viagens internacionais caiu de 3,8% em 2019 para 0,7% em 2021. Dos 12,3 milhões de viagens analisadas no ano passado, 99,3% tiveram trajeto nacional. Em 2020, as viagens dentro do país representaram 98,0% dos 13,6 milhões de viagens detalhadas pela pesquisa"
+
+// console.log(texto)
+
+console.log(8 * null)
+
+console.log("5" - 1)
+
+console.log("5" + 1)
+
+console.log("five" * 2)
+
+
