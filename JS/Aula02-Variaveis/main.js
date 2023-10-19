74e4b8d2dc3448b2e180f7f456d2f29eeca2b7dc
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-10-18 21:53:30 -0300

    continuacao da segunda aula de js

--- JS/Aula02-Variaveis/main.js
+++ JS/Aula02-Variaveis/main.js
@@ -1,36 +1,36 @@
-// var nomeDoPrimeiroAluno = "Eduardo"
+var nomeDoPrimeiroAluno = "Eduardo"
 
-// console.log(nomeDoPrimeiroAluno)
+console.log(nomeDoPrimeiroAluno)
 
-// let numeroDeAprovados = 5;
+let numeroDeAprovados = 5;
 
-// numeroDeAprovados = 7
+numeroDeAprovados = 7
 
-// console.log(numeroDeAprovados)
+console.log(numeroDeAprovados)
 
-// console.clear()
+console.clear()
 
-// var nomeAluno = "Felipe"
+var nomeAluno = "Felipe"
 
-// nomeAluno = "Victor"
+nomeAluno = "Victor"
 
-// console.log(nomeAluno)
+console.log(nomeAluno)
 
-// const nomeSiteWeb = "facebook"
+const nomeSiteWeb = "facebook"
 
 // nomeSiteWeb = "facebak"
 
-// var numero01 = 15;
+var numero01 = 15;
 
-// var numero02 = 8;
+var numero02 = 8;
 
-// var soma = numero01 + numero02;
+var soma = numero01 + numero02;
 
-// console.log(soma)
+console.log(soma)
 
-// var texto = "A proporção de viagens internacionais caiu de 3,8% em 2019 para 0,7% em 2021. Dos 12,3 milhões de viagens analisadas no ano passado, 99,3% tiveram trajeto nacional. Em 2020, as viagens dentro do país representaram 98,0% dos 13,6 milhões de viagens detalhadas pela pesquisa"
+var texto = "A proporção de viagens internacionais caiu de 3,8% em 2019 para 0,7% em 2021. Dos 12,3 milhões de viagens analisadas no ano passado, 99,3% tiveram trajeto nacional. Em 2020, as viagens dentro do país representaram 98,0% dos 13,6 milhões de viagens detalhadas pela pesquisa"
 
-// console.log(texto)
+console.log(texto)
 
 console.log(8 * null)
 
@@ -39,5 +39,29 @@ console.log("5" - 1)
 console.log("5" + 1)
 
 console.log("five" * 2)
+
+console.clear()
+
+var incremento = 2;
+
+incremento++
+incremento+=7
+
+console.log(incremento)
+
+var imparPar = 6%2
+
+console.log("Caso o resultado for 1, será ímpar. Se o resultado for par, será 0. Esse será o resultado:" + imparPar)
+
+// console.clear()
+
+var comparacao = 2 > 3
+
+console.log("3 é menor que 2? " + comparacao) 
+
+console.clear()
+
+
+
 
 
