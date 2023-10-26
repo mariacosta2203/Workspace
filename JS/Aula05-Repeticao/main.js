d016f509ff97a8a910ccdaf5062fcf46852a8a05
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-10-25 21:47:57 -0300

    licao 05 while, do-while e for

---
+++ JS/Aula05-Repeticao/main.js
@@ -0,0 +1,33 @@
+
+/* 
+let contador = 1
+
+while(contador <= 10){
+    console.log(`O número do contador é ${contador}`)
+    contador++;
+}
+console.clear()
+
+let contador2 = 10
+
+while(contador2 >= 0){
+    console.log(`O número do contador é ${contador2}`)
+    contador2--;
+}
+console.clear()
+
+let contador = 1
+do {
+    debugger
+    console.log(`O número do contador é ${contador}`)
+    contador++;
+    }while(contador <= 10)
+console.clear()
+
+**for(let contador = 1; contador <= 10; contador++){
+    console.log(`O número do contador é  
+    ${contador}`)
+}
+
+
+*/
