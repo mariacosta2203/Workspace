27c1c677df37acd53ae5bf97fb8a5ae37702b44e
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-10-29 20:09:37 -0300

    licao 05 js casa

--- JS/Aula05-Repeticao/ExercicioCasa/main.js
+++ JS/Aula05-Repeticao/ExercicioCasa/main.js
@@ -1,16 +1,13 @@
-/* 
-var contador = 2
 
-while(${contador} < ){
-    debugger
+var contador = 0
+var tabuada = 2
 
+// while(contador <= 10){
+//     // debugger
+//     console.log(`${tabuada} x ${contador} = ${tabuada * contador}`);
+//     contador++;
+// }
 
-    contador++
-}
-
-
-
-
-
-
-*/
+for(contador; contador <=10; contador++){
+    console.log(`${tabuada} x ${contador} = ${tabuada * contador}`)
+};
