c3d593d844ed39791f72c72e896da8df44ee248e
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-11-04 13:44:13 -0300

    exercicio casa js 07 array de objetos

---
+++ JS/Aula07-ArrayObjetos/ExercicioCasa/main.js
@@ -0,0 +1,47 @@
+const aluno = [
+    {
+        nome:'Ana',
+        idade:17,
+        nota:8,
+        ano:'2°B'
+    },
+
+    {
+        nome:'Bruno',
+        idade:16,
+        nota:6,
+        ano:'2°C'
+    },
+
+    {
+        nome:'Veronica',
+        idade:16,
+        nota:9,
+        ano:'2°C'   
+    },
+
+    {
+        nome:'Marta',
+        idade:15,
+        nota:5,
+        ano:'3°C'
+    },
+
+    {
+        nome:'Brenno',
+        idade:15,
+        nota:5,
+        ano:'3°C'
+    },
+
+    {
+        nome:'Maria',
+        idade:14,
+        nota:4,
+        ano:'1°F'
+    }
+]
+console.log(aluno)
+console.log(aluno[1])
+console.log(aluno[5].nota)
+console.log(JSON.stringify(aluno))
