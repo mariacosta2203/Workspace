c7f066cbd43cedcf5d985c4e87fa65928b6accee
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-11-04 12:52:57 -0300

    licao js exerciciocasa 08

---
+++ JS/Aula08-Objetos/ExercicioCasa/main.js
@@ -0,0 +1,16 @@
+function alertCookie(){
+    console.log('Você quer um cookie')
+}
+
+const alertSucesso = () => {
+    console.log('Mensagem gerada com sucesso')
+}
+
+const notebook = {
+    marca: 'Samsung XR-T',
+    cor: 'Rosa',
+    peso: '2kg',
+    memoriaRam: '32gb',
+    SistemaOperacional: 'Linux'
+}
+console.log(notebook)
