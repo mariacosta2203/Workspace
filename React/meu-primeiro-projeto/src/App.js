d23641dc89d9067ba79f79b74889b6d51a280da0
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-11-16 21:41:43 -0300

    aprendendo react

---
+++ React/meu-primeiro-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Exemplo from './components/Exemplo';
+
+function App() {
+  return (
+    <div className="App">
+      <Exemplo />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/meu-primeiro-projeto/src/components/Exemplo.jsx
@@ -0,0 +1,5 @@
+ import React from "react";
+
+ const socorroTocolo = () => <h1>Estou sofrendo para aprender React</h1>;
+
+ export default socorroTocolo;
