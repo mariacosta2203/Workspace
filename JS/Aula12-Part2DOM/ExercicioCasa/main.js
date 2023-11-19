c3cfd0876d241abccd4abaa3e86a36b63afd83a3
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2023-11-19 18:24:05 -0300

    terminando atividade 12

--- JS/Aula12-Part2DOM/ExercicioCasa/main.js
+++ JS/Aula12-Part2DOM/ExercicioCasa/main.js
@@ -1,22 +1,37 @@
-// let odioNaoVai = document.getElementsByClassName('b_12')
-//     for(let t = 0; t <= odioNaoVai.length; t++) {
-//     odioNaoVai[t].style.backgroundcolor = '#BFBFBF';
-//     odioNaoVai[t].style.border = 'solid 2px #D9BC66';
-//     odioNaoVai[t].style.padding = '5rem';
-//     odioNaoVai[t].style.margin = '7rem';
-//     }
 
-const podeSer = () =>{
+const estilizar = document.getElementsByClassName('b_12')
+    for(let x = 0; x < estilizar.length; x++){
+        estilizar[x].style.backgroundColor = '#BFBFBF';
+        estilizar[x].style.border = 'solid 2px #D9BC66';
+        estilizar[x].style.margin = '7rem';
+        estilizar[x].style.padding = '5rem';
+    }
+
+function imagem() {
+    document.getElementById("imgAdd").innerHTML = "<img src='imagens/umbrellaacademy.jpg' width='200'>"
+
+    document.body.style.backgroundColor = '#262525'
+}
+
+
+const podeSer = () => {
     let entraNome = prompt('Por favor insira o seu nome: ');
-    let span = document.getElementById('spanBotao')         
+    let span = document.getElementById('spanBotao')
     console.log(entraNome)
 
     span.innerHTML = `Olá ${entraNome} Bem-vindo a nossa academia`
 }
 
-const tabuada = () =>{
+const tabuada = () => {
     let entraNumero = prompt('Por favor insira um número: ')
-    let tabu = document.getElementsByClassName('')
+    let tabu = document.getElementById('botaoTabuada')
+    console.log(entraNumero)
+
+    var contador = 0
+
+    for (contador; contador <= 10; contador++) {
+        tabu.innerHTML += `${entraNumero} x ${contador} = ${entraNumero * contador} <br>`
+    };
 }
 
 
